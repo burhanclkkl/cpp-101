3043311d6e6a125044d932cf2a12d77a3568be35
SS' M <burhan12celikko@gmail.com>
2023-05-23 01:53:56 +0300

    C++

---
+++ day_12/day_12.cpp
@@ -0,0 +1,20 @@
+//Degiskenler
+#include <iostream>
+
+int main()
+{
+	int sayi1 = 11;
+	int sayi2 = 13;
+	std::cout << "Toplam: " << sayi1 + sayi2 << std::endl;
+
+	int sayi3 = 14;
+	int sayi4 = 15;
+	int sonuc = sayi3 + sayi4;
+	std::cout << "Toplam: " << sonuc << std::endl;
+
+	int sayi5, sayi6, sonuc2;
+	sayi5 = 11;
+	sayi6 = 17;
+	sonuc2 = sayi5 + sayi6;
+	std::cout << "Toplam: " << sonuc2 << std::endl;
+}
